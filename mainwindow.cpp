bef23bdfe626191488a9b976a903dd8b1c1beb70
bcat <v01z@mail.ru>
2022-01-23 21:17:34 +0300

    begin task1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "./task3.h"
+#include <QtMath> // for qSqrt()
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -35,5 +36,33 @@ void MainWindow::on_task1Button_clicked()
     //https://nicknixer.ru/programmirovanie/programma-dlya-resheniya-kvadratnyx-uravnenij-na-c/
     //a, b and c will get using inputboxes or creating new form.
     //or maybe using that table from metodi4ka (better variant, I suppose; no form need)
+
+    double a{}, b{}, c{}, D{}, x1{}, x2{};
+
+    a = 1;
+    b = 2;
+    c = 1;
+
+    D = b * b - 4 * a * c;
+
+    if (D > 0){
+        x1 = ((-b) + qSqrt(D)) / (2 * a);
+        x2 = ((-b) - qSqrt(D)) / (2 * a);
+        //std::cout x1, x2
+    }
+
+    if (D == 0){
+
+        x1 = -(b / (2 * a));
+//	    cout << "x1 = x2 = " << x1 << "\n";
+    }
+
+    if (D < 0){
+//        cout << "D < 0, Действительных корней уравнения не существует";
+    }
+
+
+
+
 }
 
